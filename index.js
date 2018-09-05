799067aa1f2af6e016a295be6d12f67fc70cb707
Gigi M <mmgamao1@gmail.com>
2018-09-05 00:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ var height = "70";
 module.exports = { name, height
 };
  
-if (name === "Susan") {
-    console.log("The name is correct");
-} else {
-    console.log("Expected the name to be Susan");
-}
+
 
 if (height === "70") {
     console.log("The height is correct");
